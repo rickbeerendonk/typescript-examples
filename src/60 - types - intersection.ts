ae6406867c6058d5d4def8976ea9d145a2faf885
Rick Beerendonk <rick@beerendonk.com>
2020-06-18 09:29:22 +0200

    Add "types - intersection" example.

---
+++ src/60 - types - intersection.ts
@@ -0,0 +1,23 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+type First = {
+  same: string;
+  unique1: number;
+};
+
+type Second = {
+  same: string;
+  unique2: number;
+};
+
+type Intersection = First & Second;
+
+let intersection: Intersection = {
+  same: 'intersection',
+  unique1: 1,
+  unique2: 2
+};
+
+console.log(intersection);
+// { same: 'intersection', unique1: 1, unique2: 2 }
