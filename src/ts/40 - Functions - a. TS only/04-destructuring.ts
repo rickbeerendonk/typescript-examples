560a075c9a1181318ad4469f882dc61bbb916184
Rick Beerendonk <rick@beerendonk.com>
2020-06-29 01:59:17 +0200

    Improved example.

---
+++ src/ts/40 - Functions - a. TS only/04-destructuring.ts
@@ -0,0 +1,22 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+type ObjWithName = {
+  name: string;
+};
+
+// Declarations
+function five(a: ObjWithName): string;
+function five(a: string, b: number): string;
+// Implementation
+function five(a: string | { name: string }, b?: number): string {
+  return b ? <string>a + b.toString() : (<ObjWithName>a).name;
+}
+
+console.log(five('test', 123)); // test123
+
+console.log(five({ name: 'test' })); // test
+
+// Following lines don't compile
+//console.log(five({ name: 'test' }, 123));
+//console.log(five(true)); // illegal
