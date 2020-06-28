becad4532273d9d1de231e74af81e6ff14ed0a1d
Rick Beerendonk <rick@beerendonk.com>
2020-06-28 21:39:36 +0200

    Add ES2017 examples.

---
+++ src/js/70 - Promises/12-async-await-promise-all.js
@@ -0,0 +1,31 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2017 Rick Beerendonk          !*/
+
+/* eslint no-unused-vars:warn */
+/* eslint-disable @typescript-eslint/no-floating-promises */
+
+// ES 2017
+
+const p1 = () =>
+  new Promise(function (resolve, reject) {
+    setTimeout(() => resolve('Resolved 1'), 2000);
+  });
+const p2 = () =>
+  new Promise(function (resolve, reject) {
+    setTimeout(() => resolve('Resolved 2'), 1000);
+  });
+const p3 = () =>
+  new Promise(function (resolve, reject) {
+    setTimeout(() => reject('Rejected 3'), 1500);
+  });
+
+(async function () {
+  try {
+    const arr = await Promise.all([p1(), p2(), p3()]);
+    console.log('Success: ' + JSON.stringify(arr));
+  } catch (err) {
+    console.log('Fail:', err);
+  }
+})();
+
+console.log('End of file...');
