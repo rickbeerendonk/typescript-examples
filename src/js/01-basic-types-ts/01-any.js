9489fcc42651c0c85ca92695c401d69c5cdfde43
Rick Beerendonk <rick@beerendonk.com>
2020-06-28 19:07:00 +0200

    Add JS version.

---
+++ src/js/01-basic-types-ts/01-any.js
@@ -0,0 +1,15 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+let a;
+
+// Can be anything
+
+a = true;
+console.log('Boolean: ', a); // true
+
+a = 123;
+console.log('Number: ', a); // 123
+
+a = 'text';
+console.log('String: ', a); // text
