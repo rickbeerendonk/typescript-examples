f2f4e93fadc082161f13d4905ea08da665aedced
Rick Beerendonk <rick@beerendonk.com>
2020-06-28 19:05:47 +0200

    Add "Basic Types TS - any" example.

---
+++ src/ts/01-basic-types-ts/01-any.ts
@@ -0,0 +1,19 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+let a: any;
+
+// Can be anything
+
+a = true;
+console.log('Boolean: ', a); // true
+
+a = 123;
+console.log('Number: ', a); // 123
+
+a = 'text';
+console.log('String: ', a); // text
+
+export {};
