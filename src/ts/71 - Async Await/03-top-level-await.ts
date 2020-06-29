d8acf08a3cdc6aaab473a5ab9e79c7194d2bca2c
Rick Beerendonk <rick@beerendonk.com>
2020-06-29 03:05:02 +0200

    Add top level await example.

---
+++ src/ts/71 - Async Await/03-top-level-await.ts
@@ -0,0 +1,16 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+// ES Stage 3
+
+// https://github.com/tc39/proposal-top-level-await
+
+const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+const posts = await response.json();
+
+console.log(posts);
+
+export {};
