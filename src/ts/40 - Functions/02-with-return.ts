686af2be467b49e9dabaf919be45ff249700bb2b
Rick Beerendonk <rick@beerendonk.com>
2020-06-29 15:15:30 +0200

    Explicit return type.

--- src/ts/40 - Functions/02-with-return.ts
+++ src/ts/40 - Functions/02-with-return.ts
@@ -1,7 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2018 Rick Beerendonk          !*/
 
-function add(a: number, b: number) {
+function add(a: number, b: number): number {
   return a + b;
 }
 
