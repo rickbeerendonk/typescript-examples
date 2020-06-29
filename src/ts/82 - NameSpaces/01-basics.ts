83abf10eb109a4e12b4be8f2a89565b773a3d2ad
Rick Beerendonk <rick@beerendonk.com>
2020-06-29 11:24:59 +0200

    Add namespaces example.

---
+++ src/ts/82 - NameSpaces/01-basics.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+
+namespace MyNameSpace {
+  export function foo(): void {
+    console.log('foo');
+  }
+}
+
+MyNameSpace.foo();
+
+export {};
