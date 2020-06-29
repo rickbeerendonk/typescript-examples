ea75226503051d2b1c9cc317ac04862f27dca258
Rick Beerendonk <rick@beerendonk.com>
2020-06-29 15:15:51 +0200

    Extra example. Move of results needed.

--- src/ts/40 - Functions/03-less-arguments-than-parameters.ts
+++ src/ts/40 - Functions/03-less-arguments-than-parameters.ts
@@ -1,11 +1,12 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2018 Rick Beerendonk          !*/
 
-function test(a: boolean, b?: undefined) {
-  console.log(a); // true
-  console.log(b); // undefined
+function test(a: boolean, b?: number) {
+  console.log(a);
+  console.log(b);
 }
 
-test(true);
+test(true); // true, undefined
+test(true, 123); // true, 123
 
 export {};
