e9767a990dd4a17443fbe53908c191fa718ff4c5
Rick Beerendonk <rick@beerendonk.com>
2020-06-29 15:18:06 +0200

    Add abstract class example.

---
+++ src/ts/52 - Classes - a. TS only/06-abstract.ts
@@ -0,0 +1,54 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2015 Rick Beerendonk          !*/
+
+abstract class AbstractBase {
+  abstract other(): number;
+
+  other2(): boolean {
+    return true;
+  }
+}
+
+// Only 1 class can be extended (for multiple, user interfaces)
+class Base extends AbstractBase {
+  private name: string;
+
+  constructor(name: string) {
+    super();
+    this.name = name;
+  }
+  toString() {
+    return this.name;
+  }
+
+  other(): number {
+    return 123;
+  }
+}
+
+class Account extends Base {
+  private balance: number;
+
+  constructor(name: string) {
+    super(name);
+    this.balance = 0;
+  }
+  deposit(amount: number) {
+    this.balance += amount;
+  }
+  withdraw(amount: number) {
+    this.balance -= amount;
+  }
+  toString() {
+    return `${super.toString()}: ${this.balance}`;
+  }
+}
+
+const acc = new Account('Rick');
+acc.deposit(100);
+acc.withdraw(25);
+
+console.log(acc); // { name: 'Rick', balance: 75 }
+console.log(acc.toString()); // Rick: 75
+
+export {};
