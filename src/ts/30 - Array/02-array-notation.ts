c06cdbfecdd7e994fce89e0587c8103a478a6c9d
Rick Beerendonk <rick@beerendonk.com>
2020-06-29 15:14:42 +0200

    More realistic array example added.

--- src/ts/30 - Array/02-array-notation.ts
+++ src/ts/30 - Array/02-array-notation.ts
@@ -2,8 +2,12 @@
 /*! Copyright © 2018 Rick Beerendonk          !*/
 
 const a = [true, 2, 'three'];
-
 console.log(JSON.stringify(a)); // [true,2,"three"]
 console.log(a.length); // 3
+
+let b: number[];
+b = [4, 5];
+b = [1, 2, 3];
+console.log(JSON.stringify(b)); // [1, 2, 3]
 
 export {};
