3a0ba1f56f98db76d4cf5422e3300dbce3e9f4f6
Rick Beerendonk <rick@beerendonk.com>
2020-06-30 02:25:54 +0200

    Add generic constraints example.

---
+++ src/ts/62 - Generics (TS only)/05-constraints.ts
@@ -0,0 +1,35 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+class First {
+  prop1 = 1;
+}
+
+class Second extends First {
+  prop2 = 2;
+}
+
+class Third {
+  prop1 = 3;
+}
+
+class Fourth {
+  prop2 = 4;
+}
+
+function genericFunc<T extends First>(value: T) {
+  console.log(value);
+}
+
+// Works: Obvious
+genericFunc(new First());
+genericFunc(new Second());
+
+// Works: Less obvious
+genericFunc(new Third());
+genericFunc({ prop1: 123 });
+
+// Doesn't work
+//genericFunc(new Fourth());
+
+export {};
