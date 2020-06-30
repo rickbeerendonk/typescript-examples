d6bedd7a1c48cfd0ebd5d85f516d687eabf7aa36
Rick Beerendonk <rick@beerendonk.com>
2020-06-30 13:35:53 +0200

    Remove duplicate example.

--- src/ts/62 - Generics (TS only)/04-constraints-using-class-types.ts
+++ src/ts/62 - Generics (TS only)/04-constraints-using-class-types.ts
@@ -17,13 +17,13 @@ class Fourth {
   prop2 = 4;
 }
 
-function genericFunc<T>(c: new () => T) {
+function factory<T>(c: new () => T): T {
   return new c();
 }
 
-console.log(genericFunc(First).prop1); // 1
-console.log(genericFunc(Second).prop2); // 2
-console.log(genericFunc(Third).prop1); // 3
-console.log(genericFunc(Fourth).prop2); // 4
+console.log(factory(First).prop1); // 1
+console.log(factory(Second).prop2); // 2
+console.log(factory(Third).prop1); // 3
+console.log(factory(Fourth).prop2); // 4
 
 export {};
