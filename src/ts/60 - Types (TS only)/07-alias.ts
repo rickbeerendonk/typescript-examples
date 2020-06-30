9d2c43cbfb00ee4b5beff1499e25dea2041e2d69
Rick Beerendonk <rick@beerendonk.com>
2020-06-30 02:19:19 +0200

    Add alias example.

---
+++ src/ts/60 - Types (TS only)/07-alias.ts
@@ -0,0 +1,20 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+// Alias: New name for existing type.
+
+type Text = string;
+type TextFunc = () => string;
+type TextOrTextFunc = Text | TextFunc;
+
+function getText(val: TextOrTextFunc) {
+  if (typeof val === 'string') {
+    return val;
+  } else {
+    return val();
+  }
+}
+
+console.log(getText('test')); // test
+
+export {};
