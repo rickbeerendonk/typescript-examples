1fe04b008764b80e69f3a47aa1e55b4ca587ba83
Rick Beerendonk <rick@beerendonk.com>
2022-12-05 21:13:04 +0100

    Fix naming + uppercase.

--- src/ts/07 - Enums (TS Only)/01-numbers.ts
+++ src/ts/07 - Enums (TS Only)/01-numbers.ts
@@ -1,19 +1,19 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2019 Rick Beerendonk          !*/
 
-enum color {
+enum Color {
   'R' = 1,
   'G',
   'B' = 4
 }
 
-console.log(color.R); // 1
-console.log(color.G); // 2
-console.log(color.B); // 4
+console.log(Color.R); // 1
+console.log(Color.G); // 2
+console.log(Color.B); // 4
 
-console.log(color[1]); // R
-console.log(color[2]); // G
-console.log(color[3]); // undefined
-console.log(color[4]); // B
+console.log(Color[1]); // R
+console.log(Color[2]); // G
+console.log(Color[3]); // undefined
+console.log(Color[4]); // B
 
 export {};
--- src/ts/07 - Enums (TS Only)/02-strings.ts
+++ src/ts/07 - Enums (TS Only)/02-strings.ts
@@ -1,14 +1,14 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2019 Rick Beerendonk          !*/
 
-enum colour {
+enum Color {
   R = 'red',
   G = 'green',
   B = 'blue'
 }
 
-console.log(colour.R); // red
-console.log(colour.G); // green
-console.log(colour.B); // blue
+console.log(Color.R); // red
+console.log(Color.G); // green
+console.log(Color.B); // blue
 
 export {};
