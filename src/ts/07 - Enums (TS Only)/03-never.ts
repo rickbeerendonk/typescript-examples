a89b136c7c39325f2ac1675e74f45c184dc33cb8
Rick Beerendonk <rick@beerendonk.com>
2022-12-05 21:13:20 +0100

    Add "enums - never" example.

---
+++ src/ts/07 - Enums (TS Only)/03-never.ts
@@ -0,0 +1,28 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+/* eslint-disable no-case-declarations */
+
+enum Color {
+  R,
+  G,
+  B,
+  Y
+}
+
+const colorName = (color: Color) => {
+  switch (color) {
+    case Color.R:
+      return 'Red';
+    case Color.G:
+      return 'Green';
+    case Color.B:
+      return 'Blue';
+
+    default:
+      const _exhaustiveCheck: never = color;
+      return _exhaustiveCheck;
+  }
+};
+
+export {};
