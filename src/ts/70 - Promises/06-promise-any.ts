6010dda8f29e98e1a36706b154803e387b355255
Rick Beerendonk <rick@beerendonk.com>
2022-12-05 03:26:31 +0100

    Add Promise.any example.

---
+++ src/ts/70 - Promises/06-promise-any.ts
@@ -0,0 +1,28 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+/* eslint no-unused-vars:warn */
+/* eslint @typescript-eslint/restrict-template-expressions: warn */
+/* eslint @typescript-eslint/no-unsafe-call: warn */
+/* eslint @typescript-eslint/no-unsafe-member-access: warn */
+
+'use strict';
+
+const p1 = () =>
+  new Promise(function (resolve, reject) {
+    setTimeout(() => resolve('Resolved 1'), 3000);
+  });
+const p2 = () =>
+  new Promise(function (resolve, reject) {
+    setTimeout(() => resolve('Resolved 2'), 2000);
+  });
+const p3 = () =>
+  new Promise(function (resolve, reject) {
+    setTimeout(() => reject('Rejected 3'), 1000);
+  });
+
+Promise.any([p1(), p2(), p3()])
+  .then((arr) => console.log(`Success: ${JSON.stringify(arr)}`))
+  .catch((err) => console.log(`Fail: ${err}`));
+
+console.log('End of file...');
