7b360afda18ef1c1da627dfe7cae17ac2ac2dd03
Rick Beerendonk <rick@beerendonk.com>
2023-01-25 01:57:46 +0100

    Correct and improve example.

--- src/ts/40 - Functions - a. TS only/04-overload.ts
+++ src/ts/40 - Functions - a. TS only/04-overload.ts
@@ -10,15 +10,16 @@ function five(a: ObjWithName): string;
 function five(a: string, b: number): string;
 // Implementation
 function five(a: string | ObjWithName, b?: number): string {
-  return b ? <string>a + b.toString() : (<ObjWithName>a).name;
+  return b !== undefined ? <string>a + b.toString() : (<ObjWithName>a).name;
 }
 
-console.log(five('test', 123)); // test123
+console.log(five('test', 0)); // test123
 
 console.log(five({ name: 'test' })); // test
 
 // Following lines don't compile
 //console.log(five({ name: 'test' }, 123));
+//console.log(five('abc'));
 //console.log(five(true)); // illegal
 
 export {};
