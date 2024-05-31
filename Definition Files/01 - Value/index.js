acba49faade3d7fc6f55aa63bec3824eb9dd616e
Rick Beerendonk <rick@beerendonk.com>
2024-05-31 15:45:10 +0200

    Add comments.

--- Definition Files/01 - Value/index.js
+++ Definition Files/01 - Value/index.js
@@ -1,3 +1,8 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
 import * as lib from './lib.js';
 
 // Use things from lib here.
+
+// Code complete should now work for the `lib` module.
