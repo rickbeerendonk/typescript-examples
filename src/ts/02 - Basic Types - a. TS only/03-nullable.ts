3789a7eafa44c7d1526690297d9b4a7f4ee836cb
Rick Beerendonk <rick@beerendonk.com>
2024-06-07 07:19:32 +0200

    Create a "nullable" type example.

---
+++ src/ts/02 - Basic Types - a. TS only/03-nullable.ts
@@ -0,0 +1,8 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2024 Rick Beerendonk          !*/
+
+let a: boolean;
+let b: boolean | undefined;
+
+//console.log(a); // Error: Variable 'a' is used before being assigned.
+console.log(b);
