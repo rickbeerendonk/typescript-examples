bc1ea663873fbd6bfbea028693bcd2f86619af0a
Rick Beerendonk <rick@beerendonk.com>
2025-04-03 20:50:53 +0200

    Add more examples.

--- src/ts/62 - Generics (TS only)/01-function - a. Single.ts
+++ src/ts/62 - Generics (TS only)/01-function - a. Single.ts
@@ -5,6 +5,8 @@ function genericFunc<T>(value: T) {
   console.log(typeof value, value);
 }
 
+///////// CALLING ////////////
+
 genericFunc(1); // number 1
 genericFunc<number>(2); // number 2
 
@@ -13,5 +15,16 @@ genericFunc<string>('two'); // string two
 
 // Doesn't compile:
 //genericFunc<number>(true);
+
+///////// SPECIALIZED ////////////
+
+type MyType = typeof genericFunc<number>;
+
+const myFunc: MyType = genericFunc;
+
+myFunc(3); // number 3
+
+// Doesn't compile:
+//myFunc('four');
 
 export {};
