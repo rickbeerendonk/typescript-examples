3ec48a8520b59e5565017b1181a9897f577ebbcd
Rick Beerendonk <rick@beerendonk.com>
2025-04-03 20:47:02 +0200

    Use both a property and a method.

--- src/ts/62 - Generics (TS only)/02-interface.ts
+++ src/ts/62 - Generics (TS only)/02-interface.ts
@@ -3,16 +3,17 @@
 
 interface Intf1<T> {
   value: T;
+  method(value: T): T;
 }
 
 function log1(value: Intf1<number>) {
   console.log(typeof value, value);
 }
 
-log1({ value: 123 }); // object { value: 123 }
+log1({ value: 123, method: (x) => x }); // object { value: 123, method: [Function] }
 
 // Next lines will fail to compile:
-//log1({ value: 'test' });
-//log1({ value: true });
+//log1({ value: 'test', method: (x) => x } });
+//log1({ value: true, method: (x) => x } });
 
 export {};
